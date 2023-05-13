38b0ced55643989e9976284e109de6f54d8ec8e0
Priya Hariharan <phariha@uw.edu>
2023-05-13 14:16:20 -0700

    changes

--- plantly/src/index.js
+++ plantly/src/index.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import { initializeApp } from "firebase/app";
+
+import { BrowserRouter } from 'react-router-dom';
+
+// import CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+import '../src/index.css';
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import "react-datepicker/dist/react-datepicker.css";
+
+import App from './components/App';
+import SAMPLE_PLANTS from './data/plants.json';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBrQXocgeG7E9hYfZwmSqDvzdCHpnLgAcI",
+  authDomain: "hackathon-79784.firebaseapp.com",
+  projectId: "hackathon-79784",
+  storageBucket: "hackathon-79784.appspot.com",
+  messagingSenderId: "58113007538",
+  appId: "1:58113007538:web:9be556b3f696dc4385ceab",
+  measurementId: "G-54D6FS9CL0"
+};
+
+const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+      <App plants={SAMPLE_PLANTS}/>
+    </BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
