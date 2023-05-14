6f92c2cc8fd2d7f727e903b415e0f7fe595177c5
Saimanasvi <smcharu@uw.edu>
2023-05-14 09:36:53 -0700

    transportation page edits

--- plantly/src/components/Transportation.js
+++ plantly/src/components/Transportation.js
@@ -1,38 +1,45 @@
 import React, { useRef, useEffect } from 'react';
 
-
 export function TransportationPage() {
-  const paraOneRef = useRef(null);
-  const paraTwoRef = useRef(null);
-  const paraThreeRef = useRef(null);
-
-  useEffect(() => {
-    const observer = new IntersectionObserver(entries => {
-      entries.forEach(entry => {
-        if (entry.intersectionRatio > 0) {
-          entry.target.classList.add('animate');
-        } else {
-          entry.target.classList.remove('animate');
-        }
-      });
-    });
-
-    observer.observe(paraOneRef.current);
-    observer.observe(paraTwoRef.current);
-    observer.observe(paraThreeRef.current);
-
-    return () => {
-      if (paraOneRef.current) observer.unobserve(paraOneRef.current);
-      if (paraTwoRef.current) observer.unobserve(paraTwoRef.current);
-      if (paraThreeRef.current) observer.unobserve(paraThreeRef.current);
-    };
-  }, []);
-
-
-
+  
   return (
     <>
-      <div className="home-body">
+      <body  className = "body-recycle">
+                    <h1 className="recycle-h1">Transportation</h1>
+                    <div className="container-recycle">
+                        <div className="image-recycle">
+                            <img src="https://i.guim.co.uk/img/media/2c187328c1384d02c5ecba548478fbd2af8cf674/48_29_3079_1847/master/3079.jpg?width=1200&quality=85&auto=format&fit=max&s=bf337dc1230054c7a51d86095bd967cb" alt="tree picture"></img>
+                        </div>
+                        <div className="content-recycle">
+                            <p>Transportation holds immense importance for college students, and embracing sustainable transportation practices is crucial for promoting a more environmentally conscious lifestyle. Opting for sustainable transportation options, such as biking, walking, or utilizing public transportation, not only reduces carbon emissions and mitigates climate change but also fosters a healthier and more active lifestyle. By prioritizing sustainable transportation, college students can contribute to creating a greener campus environment, minimize traffic congestion, and inspire others to make eco-friendly choices.</p>
+                        </div>
+                    </div>
+
+                    <div className = "container-recycle2"> 
+                        <h1 className = "recycle-h2">Alternative Forms of Transportation</h1>
+
+                        <div className="image-section-recycle">
+                          <div className="image-container-recycle">
+                            <img className="rounded-image-recycle" src="https://kingcounty.gov/~/media/depts/metro/travel-options/bus/rapidride/v2/rapidride-featured-image" alt="Image 1" />
+                            <div className="caption-recycle">Bus</div>
+                          </div>
+                          <div className="image-container-recycle">
+                            <img className="rounded-image-recycle" src="https://cdn.geekwire.com/wp-content/uploads/2018/11/limebikes2.jpg" alt="Image 2" />
+                            <div className="caption-recycle">Lime Bikes and Scooters</div>
+                          </div>
+                          <div className="image-container-recycle">
+                            <img className="rounded-image-recycle" src="https://kuow-prod.imgix.net/store/9a6e5ff0d9602303fff8a7805e751f7f.jpg" alt="Image 3" />
+                            <div className="caption-recycle">Light Rail</div>
+                          </div>
+                          <div className="image-container-recycle">
+                            <img className="rounded-image-recycle" src="https://sdotblog.seattle.gov/wp-content/uploads/sites/10/2021/02/49561821172_1d1efc37f2_b-e1613679849945.jpg" alt="Image 3" />
+                            <div className="caption-recycle">Walking</div>
+                          </div>
+                        </div>
+                    </div>
+                </body>
+
+      {/* <div className="home-body">
         <div className="container-fluid">
           <div className="row">
             <img className="tree-picture" src="https://i.guim.co.uk/img/media/2c187328c1384d02c5ecba548478fbd2af8cf674/48_29_3079_1847/master/3079.jpg?width=1200&quality=85&auto=format&fit=max&s=bf337dc1230054c7a51d86095bd967cb" alt="tree picture"></img>
@@ -56,7 +63,7 @@ export function TransportationPage() {
             </div>
           </div>
         </div>
-      </div>
+      </div> */}
     </>
   );
 }
