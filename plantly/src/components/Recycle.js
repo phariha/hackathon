fdd4f9097c85f2e94bc2357765b41c4b6989a66a
Saimanasvi <smcharu@uw.edu>
2023-05-13 22:30:42 -0700

    recycle page edits

--- plantly/src/components/Recycle.js
+++ plantly/src/components/Recycle.js
@@ -6,10 +6,10 @@ export function RecyclePage() {
                 <body>
                     <div className="container-recycle">
                         <div className="image-recycle">
-                            <img src="path_to_your_image.jpg" alt="Image"></img>
+                            <img src="img/recycle.jpeg" alt="Image" />
                         </div>
                         <div className="content-recycle">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eleifend lorem non fringilla aliquam. Nullam ultrices, nunc nec dictum elementum, est lectus gravida odio, sit amet congue tortor quam vel lacus.</p>
+                            <p>The importance of recycling is closely tied to the concept of sustainability. Recycling is a key component of sustainable practices as it promotes the efficient use of resources and minimizes waste generation. By recycling materials such as paper, plastic, glass, and metals, we can reduce the demand for raw materials and energy-intensive extraction processes. This conserves natural resources, reduces pollution and greenhouse gas emissions, and helps mitigate the negative impacts of climate change. Additionally, recycling encourages a circular economy where materials are reused, prolonging their lifespan and reducing the need for constant production. Embracing recycling as part of our daily lives is essential for building a more sustainable future, where we can preserve the planet's resources for generations to come.</p>
                         </div>
                     </div>
                 </body>
