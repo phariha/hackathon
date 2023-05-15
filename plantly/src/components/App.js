a4d108d83afbc259a29ec17d6c6f30bc35c53913
Priya Hariharan <phariha@uw.edu>
2023-05-15 16:26:49 -0700

    app

--- plantly/src/components/App.js
+++ plantly/src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { Navigate } from 'react-router-dom';
 import { PlantNav } from './Nav.js';
